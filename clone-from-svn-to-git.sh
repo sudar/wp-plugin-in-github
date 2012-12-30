d63c3323cca674608cc3b7f9fc3ac7252a5cde21
Sudar <sudar@sudarmuthu.com>
2012-12-30 14:31:56 +0530

    Added git rebase trunk which fixed the empty repo issue

--- clone-from-svn-to-git.sh
+++ clone-from-svn-to-git.sh
@@ -11,7 +11,7 @@
 
 # default configurations
 PLUGIN_NAME="posts-by-tag"
-AUTHORS_FILE="/home/sudar/Dropbox/code/wp/wp-plugin-in-gitbub/authors.txt"
+AUTHORS_FILE="/home/sudar/Dropbox/code/wp/wp-plugin-in-github/authors.txt"
 SVN_USERNAME="sudar"
 GITHUB_REPO="git@github.com:$SVN_USERNAME/$PLUGIN_NAME.git"
 
@@ -34,14 +34,15 @@ done
 
 # Get the first revision number when this Plugin was checked in first
 REVISION=$(svn log -r 1:HEAD --limit 1 http://plugins.svn.wordpress.org/$PLUGIN_NAME | awk 'NR==2{print $1;exit;}')
-echo "Plugin was first checked in at revision: $REVISION"
+echo ">>>Plugin was first checked in at revision: $REVISION"
 
-git svn clone -s -A $AUTHORS_FILE --no-minimize-url --username=$SVN_USERNAME http://plugins.svn.wordpress.org/$PLUGIN_NAME -$REVISION
-echo "Repo cloned. Let's fetch it"
+git svn clone -s -$REVISION -A $AUTHORS_FILE --no-minimize-url --username=$SVN_USERNAME http://plugins.svn.wordpress.org/$PLUGIN_NAME
+echo ">>>Repo cloned. Let's fetch it"
 
 cd $PLUGIN_NAME
 git svn fetch
-echo "Fetched the content from svn. Adding repo to github"
+git rebase trunk
+echo ">>>Fetched the content from svn. Adding repo to github"
 
 git remote add origin $GITHUB_REPO
 
@@ -50,4 +51,4 @@ git ci -am "Renamed readme.txt to readme.md, so that github can parse it"
 
 git push -u origin master
 
-echo "Done"
+echo ">>>Done"
